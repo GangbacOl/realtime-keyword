5ae86c3686c666d6e63e0c5857fbe6d602a745a7
GangbacOl <gangyeonjo336@gmail.com>
2021-06-17 11:33:23 +0900

    chore: 키워드 리스트 생성하는 함수 작성

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
-const list = document.querySelector('.list-container');
+const mainContainer = document.querySelector('.main-container');
+const readMoreBtn = document.querySelector('.read-more');
 
 const handleCollapse = (classList) => {
     if (classList.value.indexOf('not-open') !== -1) {
@@ -10,10 +11,14 @@ const handleCollapse = (classList) => {
     }
 };
 
-chrome.storage.local.get(['hotItems'], ({ hotItems }) => {
-    console.log(hotItems);
+const generateKeywordList = (keywords) => {
+    const keywordsWrapper = document.createElement('div');
+    const date = document.createElement('h2');
+    keywordsWrapper.classList.add('list-container');
+    date.classList.add('date');
+    console.log(new Date());
 
-    hotItems[1].forEach((item, idx) => {
+    keywords.forEach((keyword, idx) => {
         const wrapper = document.createElement('div');
         const title = document.createElement('h2');
         const newsWrapper = document.createElement('div');
@@ -23,17 +28,32 @@ chrome.storage.local.get(['hotItems'], ({ hotItems }) => {
         const author = document.createElement('b');
 
         wrapper.classList.add('list-item', 'not-open');
-        title.innerText = item.title['#text'];
+        title.innerText = keyword.title['#text'];
         title.onclick = () => handleCollapse(wrapper.classList);
-        thumbnail.src = item.picture['#text'];
-        link.href = item.news_items[0].url['#text'];
-        link.innerText = item.news_items[0].title['#text'];
-        author.innerText = item.news_items[0].source['#text'];
+        thumbnail.src = keyword.picture['#text'];
+        link.href = keyword.news_items[0].url['#text'];
+        link.target = '_blank';
+        link.innerHTML = keyword.news_items[0].title['#text'];
+        author.innerText = keyword.news_items[0].source['#text'];
 
         description.appendChild(link);
+
         newsWrapper.append(thumbnail, description, author);
         wrapper.append(title, newsWrapper);
 
-        list.appendChild(wrapper);
+        keywordsWrapper.appendChild(wrapper);
     });
+    mainContainer.appendChild(keywordsWrapper);
+};
+
+chrome.storage.local.get(['hotItems'], ({ hotItems }) => {
+    let hotItemsIdx = 0;
+    const message = document.createElement('p');
+    console.log(hotItems);
+
+    message.classList.add('message');
+    message.innerText = '아직 수집된 데이터가 없습니다!';
+    readMoreBtn.onclick = () => generateKeywordList(hotItems[++hotItemsIdx]);
+
+    mainContainer.appendChild(hotItems[0].length < 1 ? message : generateKeywordList(hotItems[hotItemsIdx]));
 });
