ef06d43cf93af92f093b49f139b8f55ded5c6fb4
SeungBo <47034129+SecretJuJu@users.noreply.github.com>
2021-09-11 18:10:20 +0900

    refactor: utils var 선언 제거 (let, const 로 선언)

--- js/background/utils.js
+++ js/background/utils.js
@@ -1,14 +1,14 @@
 const STANDARD_DAY = new Date("1970-01-01 00:00:00");
 
 export const parseXML = (data) => {
-  let xml, tmp;
+  let xml;
   if (!data || typeof data !== "string") {
     return null;
   }
   try {
     if (window.DOMParser) {
       // Standard
-      tmp = new DOMParser();
+      const tmp = new DOMParser();
       xml = tmp.parseFromString(data, "text/xml");
     } else {
       // IE
@@ -31,15 +31,15 @@ export const parseXML = (data) => {
 
 export const xmlToJson = (xml) => {
   // Create the return object
-  var obj = {};
+  let obj = {};
 
   if (xml.nodeType == 1) {
     // element
     // do attributes
     if (xml.attributes.length > 0) {
       obj["@attributes"] = {};
-      for (var j = 0; j < xml.attributes.length; j++) {
-        var attribute = xml.attributes.item(j);
+      for (let j = 0; j < xml.attributes.length; j++) {
+        let attribute = xml.attributes.item(j);
         obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
       }
     }
@@ -50,14 +50,14 @@ export const xmlToJson = (xml) => {
 
   // do children
   if (xml.hasChildNodes()) {
-    for (var i = 0; i < xml.childNodes.length; i++) {
-      var item = xml.childNodes.item(i);
-      var nodeName = item.nodeName;
+    for (let i = 0; i < xml.childNodes.length; i++) {
+      const item = xml.childNodes.item(i);
+      const nodeName = item.nodeName;
       if (typeof obj[nodeName] == "undefined") {
         obj[nodeName] = xmlToJson(item);
       } else {
         if (typeof obj[nodeName].push == "undefined") {
-          var old = obj[nodeName];
+          let old = obj[nodeName];
           obj[nodeName] = [];
           obj[nodeName].push(old);
         }
